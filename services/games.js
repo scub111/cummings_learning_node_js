451264490c2a951b9b98fb319427d050672ea8ca
scub111 <scub111@mail.ru>
2018-11-13 10:27:48 +0300

    Test commit.

--- services/games.js
+++ services/games.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 const games = [];
 let nextId = 1;
@@ -29,7 +29,7 @@ module.exports.create = (userId, word) => {
    const newGame = new Game(nextId++, userId, word);
    games.push(newGame);
    return newGame;
-}
+};
 
 module.exports.get = (id) => games.find(game => game.id === parseInt(id, 10));
 
